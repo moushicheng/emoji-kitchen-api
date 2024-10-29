c4068b492898ded60677b8597f0dc64232c44490
moushicheng <moushicheng@outlook.com>
2024-10-29 21:41:07 +0800

    chore: port & pm2

---
+++ pm2.config.cjs
@@ -0,0 +1,23 @@
+// pm2.config.js
+module.exports = {
+  apps: [
+    {
+      name: 'emoji', // 应用名称
+      script: 'node', // 使用 ts-node 运行
+      args: 'index.js', // 你的 TypeScript 文件
+      instances: '1', // 使用所有可用的 CPU 核心
+      autorestart: true, // 自动重启
+      watch: false, // 是否监视文件变动
+      env: {
+        NODE_ENV: 'development', // 开发环境变量
+        isDev: 'false',
+        PORT: 3003 // 设置端口为 3001
+      },
+      env_production: {
+        NODE_ENV: 'production', // 生产环境变量
+        isDev: 'false',
+        PORT: 3003 // 设置端口为 3001
+      }
+    }
+  ]
+}
